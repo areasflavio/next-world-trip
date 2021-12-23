3150c0b649fcb1120379da20537f3b79b495cff3
Flávio Arêas <vflavio9@gmail.com>
2021-12-23 11:20:14 -0300

    feat: add back-button prop to Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,21 @@
-import { Center, Image } from '@chakra-ui/react';
+import { Button, Center, Image } from '@chakra-ui/react';
+import Link from 'next/link';
 
-export function Header() {
+interface HeaderProps {
+  hasBackButton?: boolean;
+}
+
+export function Header({ hasBackButton }: HeaderProps) {
   return (
-    <Center as="header" h={['12', '24']}>
+    <Center as="header" h={['12', '24']} position="relative">
+      {hasBackButton && (
+        <Link href="/" passHref>
+          <Button bg="transparent" position="absolute" left={['36']}>
+            <Image src={`/assets/icons/back.svg`} alt="Back" />
+          </Button>
+        </Link>
+      )}
+
       <Image src="/assets/logo.svg" alt="worldtrip" />
     </Center>
   );
